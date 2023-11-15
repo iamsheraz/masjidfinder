18261e5c05cb648a01f6d348e0401bc0278d8ef7
Muhammad Tariq <81058550+iamsheraz@users.noreply.github.com>
2023-11-15 17:58:55 -0600

    Adding search logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React from 'react';
+import SearchBar from './components/SearchBar/SearchBar';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Masjid Finder
-      </h1>
+      <header className="App-header">
+        <h1>
+          Masjid Finder
+        </h1>
+      </header>
+      <main>
+        <SearchBar /* props if needed */ />
+      </main>
+      <footer className="App-footer">
+        {/* Fotter content */}
+      </footer>
     </div>
   );
 }
